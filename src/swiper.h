c3f63c21c321585d11d650adf8358f43602cce33
Bee-Mar <bpmarlowe-software@protonmail.com>
2019-06-02 15:22:04 -0400

    removed unused headers

--- src/swiper.h
+++ src/swiper.h
@@ -13,24 +13,9 @@
 #include <csignal>
 #include <ctime>
 #include <iostream>
+#include <sys/wait.h>
 #include <thread>
 #include <wiringPi.h>
-
-// original C headers used
-#include <ctype.h>
-#include <poll.h>
-#include <pthread.h>
-#include <sched.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
-#include <unistd.h>
 
 #define READ 0
 #define WRITE 1
